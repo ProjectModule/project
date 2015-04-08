c06ddf735d7fdd3f5a00ff1c422fe555585cd508
Arya Kumar <aryajeswaltelerik@gmail.com>
2015-04-08 12:37:44 +0000

    Changes made by Arya Kumar (aryajeswaltelerik@gmail.com).

--- project/script/js/app.js
+++ project/script/js/app.js
@@ -21,7 +21,7 @@ var app = (function(win){
         run:function()
         {
             FB.init({
-                appId: "1396043510711208",
+                appId: "1410862485895977",
                 status:"false",
                 nativeInterface: CDV.FB,
                 useCachedDialogs: false
